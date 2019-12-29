081edd7b74cfbce49ee7e0985383ed9c39f9c052
Filip <39651273+Kotfil@users.noreply.github.com>
2019-12-29 17:17:44 +0200

    1

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className="header">
+    <img src="https://seeklogo.com/images/L/Lego-logo-749EEACA53-seeklogo.com.png">
+
+    </img>
+    </header>
+    );
+}
+
+export default Header;
---
+++ components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+return (
+    <div className="content">
+    <div>
+    <img src="https://images.unsplash.com/photo-1516641051054-9df6a1aad654?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></img>
+    </div>
+    <div>
+    ava + description
+    </div>
+    <div>
+        My post 
+        <div>
+            New post
+        </div>
+        <div>
+            Post 1
+        </div>
+        <div>
+            Post 2
+        </div>
+</div>
+</div>
+);
+}   
+
+export default Profile;
