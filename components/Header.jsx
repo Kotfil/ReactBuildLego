dc6237746315e017d4e681e4207d74b888d24973
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-12-29 18:28:17 +0200

    2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-    <header className="header">
+    <header className={s.header}>
     <img src="https://seeklogo.com/images/L/Lego-logo-749EEACA53-seeklogo.com.png">
 
     </img>
