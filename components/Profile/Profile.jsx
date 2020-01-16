c5323e7b2deaa40b8e49b29b81a634fab1b0bf85
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-01-17 00:17:06 +0200

    17

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -4,11 +4,21 @@ import MyPosts from "./MyPosts/MyPosts.jsx";
 import ProfileInfo from "./ProfileInfo/Profileinfo";
 
 const Profile = () => {
-  return (
-    <div>
+
+    let posts = [
+        {id:1, message:'Hi,how are you?', likesCount: 12},
+        {id:2, message:'Its my first post', likesCount: 11},
+        {id:3, message: 'Hello Filippe', likesCount: 11},
+        {id:4, message:'Say Hello', likesCount: 11}
+   ]
+
+   return (
+        
+        <div>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts  posts={posts} />
     </div>
+ 
   );
 };
 
