fb8523b90e9cb36fc7accbb5fa3ca65f6f98d18f
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-01-25 12:42:56 +0200

    25(0)

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts.jsx";
+import ProfileInfo from "./ProfileInfo/Profileinfo";
+
+const Profile = props => {
+  return (
+    <div>
+      <ProfileInfo />
+      <MyPosts
+        posts={props.profilePage.posts}
+        newPostText={props.profilePage.newPostText}
+        updateNewPostText={props.updateNewPostText}
+        addPost={props.addPost}
+      />
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import * as serviceWorker from './serviceWorker';
+import store from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+
+ let renderEntireTree = (state) => {
+    ReactDOM.render ( 
+        <BrowserRouter>
+            <App  
+            state={state} 
+            addPost={store.addPost.bind(store)} 
+            updateNewPostText={store.updateNewPostText.bind(store)}/>
+                </BrowserRouter>, document.getElementById('root'));
+}
+
+
+ 
+ 
+renderEntireTree(store.getState());
+
+store.subscribe(renderEntireTree);
+
+serviceWorker.unregister();
+
