5550d5d3bf80723245d0e88e7ee52001c607ae0e
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-01-25 12:45:08 +0200

    25(1)

--- components/Profile/MyPosts/MyPosts.jsx
+++ components/Profile/MyPosts/MyPosts.jsx
@@ -9,6 +9,7 @@ const MyPosts = (props) => {
 
   let newPostElement = React.createRef();
   let addPost = () => {
+      debugger;
     let text = newPostElement.current.value;
     props.addPost();
     props.updateNewPostText('');
--- redux/state.js
+++ redux/state.js
@@ -1,62 +1,120 @@
-let renderEntireTree = () =>
-{
-    console.log('State changed');
-}
-
-let state = {  
-    profilePage: {
-        posts: [
-            { id: 1, message: "Hi, How are you?", likesCount: 11},
-            { id: 2, message: "It`s my first post" ,likesCount: 11},
-            { id: 3, message: "Blabla", likesCount: 11},
-            { id: 4, message: "Dadada", likesCount: 11},
-            { id: 5, message: "YO" ,likesCount: 11},
-            { id: 6, message: "YO", likesCount: 11 }
-          ],
-          newPostText: ''
+let store = {
+    _state: {
+        profilePage: {
+            posts: [{
+                    id: 1,
+                    message: "Hi, How are you?",
+                    likesCount: 11
+                },
+                {
+                    id: 2,
+                    message: "It`s my first post",
+                    likesCount: 11
+                },
+                {
+                    id: 3,
+                    message: "Blabla",
+                    likesCount: 11
+                },
+                {
+                    id: 4,
+                    message: "Dadada",
+                    likesCount: 11
+                },
+                {
+                    id: 5,
+                    message: "YO",
+                    likesCount: 11
+                },
+                {
+                    id: 6,
+                    message: "YO",
+                    likesCount: 11
+                }
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [{
+                    id: 1,
+                    name: "Dimych"
+                },
+                {
+                    id: 2,
+                    name: "Andrew"
+                },
+                {
+                    id: 3,
+                    name: "Sveta"
+                },
+                {
+                    id: 4,
+                    name: "Sasha"
+                },
+                {
+                    id: 5,
+                    name: "Viktor"
+                },
+                {
+                    id: 6,
+                    name: "Valera"
+                }
+            ],
+            messages: [{
+                    id: 1,
+                    message: "Hi"
+                },
+                {
+                    id: 2,
+                    message: "How is your Lego"
+                },
+                {
+                    id: 3,
+                    message: "YO"
+                },
+                {
+                    id: 4,
+                    message: "YO"
+                },
+                {
+                    id: 5,
+                    message: "YO"
+                },
+                {
+                    id: 6,
+                    message: "YO"
+                }
+            ]
+        },
+        sidebar: {}
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: "Dimych" },
-            { id: 2, name: "Andrew" },
-            { id: 3, name: "Sveta" },
-            { id: 4, name: "Sasha" },
-            { id: 5, name: "Viktor" },
-            { id: 6, name: "Valera" }
-          ],
-        messages: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "How is your Lego" },
-            { id: 3, message: "YO" },
-            { id: 4, message: "YO" },
-            { id: 5, message: "YO" },
-            { id: 6, message: "YO" }
-          ]
+    getState() {
+        return this._state;
     },
-    sidebar: {}
-}
-window.state=state;
-
-    export const addPost = () => {
-    
+    _callSubscriber() {
+        console.log('State changed');
+    },
+    addPost() {
+        debugger;
         let newPost = {
             id: 5,
-            message: state.profilePage.newPostText,
+            message: this._state.profilePage.newPostText,
             likesCount: 0
         };
-        state.profilePage.posts.push(newPost);
-        state.profilePage.newPostText = '';
-              renderEntireTree(state);
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        debugger;
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 
-    export const updateNewPostText = (newText) => {
-    
-        state.profilePage.newPostText = newText;
-      renderEntireTree(state);
 }
 
-    export const subscribe = (observer) => {
-
-    } 
-export default state;
-
+export default store;
+window.store = store;
